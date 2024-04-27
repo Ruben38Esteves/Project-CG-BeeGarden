9e6c24e4d804af5524b2de7eb4cebc37ed13d4bc
Ruben Esteves <ruben38esteves@gmail.com>
2024-04-27 18:25:50 +0100

    added randomness in petals

--- project/MyFower.js
+++ project/MyFower.js
@@ -16,6 +16,17 @@ export class MyFlower extends CGFobject {
         this.receptacle_radius = receptacle_radius;
         this.stem_radius = stem_radius;
         this.stem_length = stem_length;
+        this.stem_rotations = [];
+        for(let a = 0; a < this.stem_length; a++){
+            this.stem_rotations.push(Math.floor(Math.random() * 30));
+            console.log(this.stem_rotations[a]);
+        }
+        this.petal_rotations = [];
+        this.petal_curvatures = [];
+        for(let c = 0; c < this.petal_amount + (this.stem_length -1); c++){
+            this.petal_rotations.push(Math.floor(Math.random() * 60) - 30);
+            this.petal_curvatures.push(Math.floor(Math.random() * 60) + 60);
+        }
         this.initObjects();
         this.initBuffers();
 	}
@@ -24,6 +35,7 @@ export class MyFlower extends CGFobject {
         this.petal = new MyPetal(this.scene,60,this.outside_radius-this.receptacle_radius);
         this.receptacle = new MyReceptacle(this.scene,this.receptacle_radius,30,30);
         this.stem = new MyStem(this.scene,30,30,this.stem_radius);
+        
     }
     
 
@@ -75,13 +87,17 @@ export class MyFlower extends CGFobject {
             this.stem.display();
             this.scene.popMatrix();
             for(let i = 1; i < this.stem_length;i++){
+                // petal
                 this.scene.pushMatrix();
                 this.scene.translate(0, i * 4, 0);
+                this.scene.rotate(this.degToRad(this.petal_rotations[i]),0,1,0);
                 this.scene.rotate((Math.PI/2),0,0,1);
                 this.petal.display();
                 this.scene.popMatrix();
-
+                // stem
                 this.scene.pushMatrix();
+                //this.scene.rotate(this.degToRad(this.stem_rotations[i]),1,0,0);
+                //this.scene.rotate(this.degToRad(this.stem_rotations[i]),0,0,1);
                 this.scene.translate(0, i * 4 ,0);
                 this.stem.display();
                 this.scene.popMatrix();
@@ -104,9 +120,12 @@ export class MyFlower extends CGFobject {
             this.scene.translate(0, this.stem_length * 4, 0);
             this.scene.rotate(((2*Math.PI)/this.petal_amount)*j,0,0,1);
             this.scene.translate(0,this.receptacle_radius,0);
-            this.petal.display();
+            this.scene.rotate(this.degToRad(this.petal_rotations[j + (this.stem_length -1)]),1,0,0);
+            let new_petal = new MyPetal(this.scene,this.petal_curvatures[j+ (this.stem_length -1)],this.outside_radius-this.receptacle_radius);
+            new_petal.display();
             this.scene.popMatrix();
         }
+    
     }
 
     enableNormalViz(){
